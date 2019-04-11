4c5ed31927a489b5e43b13392f1bb52c43ce6c53
Marvin Matos <marvin.matos@weightwatchers.com>
2019-04-11 16:59:22 -0400

    Fix dup code

--- directory.go
+++ directory.go
@@ -10,16 +10,6 @@ import (
 	homedir "github.com/mitchellh/go-homedir"
 )
 
-// Exists reports whether the named file or directory exists.
-func Exists(name string) bool {
-	if _, err := os.Stat(name); err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
-	}
-	return true
-}
-
 func getHome() string {
 	// Find home directory.
 	home, err := homedir.Dir()
